df0fb036f9014db44500e9280759f6af657ed4bb
ChikhaouiFaycel <faycel@octoplus.tech>
2025-10-24 04:38:12 +0100

    define swagger config and setup swagger dark theme

--- apps/stitch-connect/src/main.ts
+++ apps/stitch-connect/src/main.ts
@@ -5,15 +5,38 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app/app.module';
+
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Dexper API')
+    .setDescription('Dexper REST API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    deepScanRoutes: true,
+  });
+  const customCss = fs.readFileSync(
+    path.join(__dirname, 'assets/swagger-dark.css'),
+    { encoding: 'utf8' }
+  );
+
+  SwaggerModule.setup('api', app, document, { customCss });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
